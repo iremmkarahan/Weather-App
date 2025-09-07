9083b37c3bc7581ca4dfe30a0a5df430fc9120f6
İrem Karahan <irem.karahan@tedu.edu.tr>
2025-09-07 22:16:28 +0300

    created general view of the app

--- WeatherApp/WeatherApp/ContentView.swift
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .cyan, .cyan, .white]), startPoint: .topTrailing, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("San Francisco, CA")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+                 
+            }
         }
-        .padding()
     }
 }
 
